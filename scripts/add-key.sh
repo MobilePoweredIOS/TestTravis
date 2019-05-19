3fe31d0ab00f4e0a89eda29d6e25c6d684b4e0cf
Sassi Walid <swa@mobilepowered.fr>
2019-05-19 18:09:35 +0100

    keys

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -8,8 +8,8 @@ security unlock-keychain -p travis ios-build.keychain
 # Set keychain timeout to 1 hour for long builds
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 # Add certificates to keychain and allow codesign to access them
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "azerty" -T /usr/bin/codesign
-Security import ./scripts/certs/Certif.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "azerty" -A
+Security import ./scripts/certs/Certif.cer -k ~/Library/Keychains/ios-build.keychain -A
 # Set Key partition list
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 # Put the provisioning profile in place 
